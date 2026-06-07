d3c49da03f8a4b21d440a4d91363af2df23a30b7
Tanishka <tanishka29.work@gmail.com>
2026-06-07 13:19:42 +0530

    Update DockerPush.groovy

--- vars/DockerPush.groovy
+++ vars/DockerPush.groovy
@@ -2,5 +2,5 @@ def call(String Project, String ImageTag, String dockerhubuser){
   withCredentials([usernamePassword(credentialsId: "DockerHubCred",passwordVariable: "dockerhubPass",usernameVariable: "dockerhubUser")]) {
   sh "docker login -u ${env.dockerhubUser} -p ${env.dockerhubPass}"
 }
-   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag}"
+   sh "docker build -t ${dockerhubUser}/${ProjectName}:${ImageTag}"
 }
